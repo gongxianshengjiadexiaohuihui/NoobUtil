d430f0682949ebd5a2fd7a9d480d8ee7500a8b19
gongguanpeng@xdja.com <gongguanpeng@xdja.com>
2019-11-06 19:22:37 +0800

    签名工具类-签名功能-SM3WithSM2算法 签名工具类-验签功能-SM3WithSM2算法 签名工具类-完善代码

--- src/main/java/com/ggp/pki/signature/SignatureUtil.java
+++ src/main/java/com/ggp/pki/signature/SignatureUtil.java
@@ -1,9 +1,9 @@
 package com.ggp.pki.signature;
 
 import com.ggp.common.base.BCProvider;
+import com.ggp.common.enums.pki.SignatureAlgorithmEnum;
 import org.bouncycastle.util.encoders.Base64;
 
-import java.io.UnsupportedEncodingException;
 import java.security.*;
 
 /**
@@ -18,15 +18,15 @@ public class SignatureUtil extends BCProvider {
      * @param signatureAlgorithmName 签名算法名字
      * @param key                    私钥
      * @param source                 签名原文
-     * @return                       Base64字符串
+     * @return Base64字符串
      */
     public static String signature(String signatureAlgorithmName, PrivateKey key, String source) throws SignatureException {
         try {
             Signature signature = Signature.getInstance(signatureAlgorithmName);
             signature.initSign(key);
-            signature.update(source.getBytes("UTF-8"));
+            signature.update(source.getBytes());
             byte[] out = signature.sign();
-            return new String(Base64.encode(out), "UTF-8");
+            return new String(Base64.encode(out));
         } catch (Exception e) {
             throw new SignatureException("签名失败", e);
         }
@@ -41,7 +41,7 @@ public class SignatureUtil extends BCProvider {
      * @param sign                   签名值
      * @return
      */
-    public static boolean verify(String signatureAlgorithmName, PublicKey key, String source, String sign) throws SignatureException{
+    public static boolean verify(String signatureAlgorithmName, PublicKey key, String source, String sign) throws SignatureException {
         try {
             Signature signature = Signature.getInstance(signatureAlgorithmName);
             signature.initVerify(key);
@@ -50,5 +50,28 @@ public class SignatureUtil extends BCProvider {
         } catch (Exception e) {
             throw new SignatureException("验签失败", e);
         }
+    }
+
+    /**
+     * 签名 签名算法为SM3WithSM2
+     *
+     * @param key    私钥
+     * @param source 签名原文
+     * @return
+     */
+    public static String signatureBySm3WithSm2(PrivateKey key, String source) throws SignatureException {
+        return signature(SignatureAlgorithmEnum.SM3_WITH_SM2.name, key, source);
+    }
+
+    /**
+     * 验签 签名算法为SM3WithSM2
+     *
+     * @param key    公钥
+     * @param source 签名原文
+     * @param sign   签名值
+     * @return
+     */
+    public static boolean verifyBySm3WithSm2(PublicKey key, String source, String sign) throws SignatureException {
+        return verify(SignatureAlgorithmEnum.SM3_WITH_SM2.name, key, source, sign);
     }
 }
--- src/test/java/com/ggp/pki/signature/SignatureUtilTest.java
+++ src/test/java/com/ggp/pki/signature/SignatureUtilTest.java
@@ -30,13 +30,16 @@ public class SignatureUtilTest {
      * 签名 sm2WithSm3
      */
     @Test
-    public void should_be_no_exception_when_signature_by_sm3WithSm2(){
+    public void should_be_success_when_signature_by_sm3WithSm2() {
         boolean flag = true;
         try {
             String source = "test";
-            String result = SignatureUtil.signature(SignatureAlgorithmEnum.SM3_WITH_SM2.name, sm2PrivateKey, source);
-            System.out.println("私钥："+sm2PrivateKey);
-            System.out.println("签名结果：" + result);
+            String result1 = SignatureUtil.signature(SignatureAlgorithmEnum.SM3_WITH_SM2.name, sm2PrivateKey, source);
+            String result2 = SignatureUtil.signatureBySm3WithSm2(sm2PrivateKey, source);
+            System.out.println("私钥：" + sm2PrivateKey);
+            System.out.println("签名结果1：" + result1);
+            System.out.println("签名结果2：" + result2);
+            //TODO 同样数据两次签名结果不一致 猜测签名前随机填充了字节
         } catch (SignatureException e) {
             flag = false;
         }
@@ -47,15 +50,19 @@ public class SignatureUtilTest {
      * 验签 sm2WithSm3
      */
     @Test
-    public void should_be_success_when_verify_signature_with_sm3WithSm2(){
-        boolean flag = true;
+    public void should_be_success_when_verify_signature_with_sm3WithSm2() {
+        boolean flag1;
+        boolean flag2;
         try {
             String source = "test";
-            String sign = SignatureUtil.signature(SignatureAlgorithmEnum.SM3_WITH_SM2.name, sm2PrivateKey, source);
-            flag = SignatureUtil.verify(SignatureAlgorithmEnum.SM3_WITH_SM2.name,sm2PublicKey,source,sign);
+            String sign1 = SignatureUtil.signature(SignatureAlgorithmEnum.SM3_WITH_SM2.name, sm2PrivateKey, source);
+            String sign2 = SignatureUtil.signatureBySm3WithSm2(sm2PrivateKey, source);
+            flag1 = SignatureUtil.verify(SignatureAlgorithmEnum.SM3_WITH_SM2.name, sm2PublicKey, source, sign1);
+            flag2 = SignatureUtil.verifyBySm3WithSm2(sm2PublicKey, source, sign2);
         } catch (SignatureException e) {
-            flag = false;
+            flag1 = flag2 = false;
         }
-        Assert.assertTrue(flag);
+        Assert.assertTrue(flag1 && flag2);
     }
+
 }
